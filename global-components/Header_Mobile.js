4cbc2928d1a9763442c74c5c67b167b9d7ba7cb9
Debashis Ray <debashish.ray@strativ.se>
2022-10-30 22:00:54 +0600

    search added on mobile

--- global-components/Header_Mobile.js
+++ global-components/Header_Mobile.js
@@ -1,89 +1,112 @@
-import React, { useState } from 'react'
-import Image from 'next/image'
-import Logo from '../public/tvcl.svg'
-import HamburgerIcon from '../images/icons/HamburgerIcon.svg';
-import SearchIcon from '../images/icons/SearchIcon.svg';
-import SettingsIcon from '../images/icons/SettingsIcon';
-import TvIcon from '../images/icons/TvIcon.svg';
+import React, { useState } from "react";
+import Image from "next/image";
+import Logo from "../public/tvcl.svg";
+import HamburgerIcon from "../images/icons/HamburgerIcon.svg";
+import SearchIcon from "../images/icons/SearchIcon.svg";
+import SettingsIcon from "../images/icons/SettingsIcon";
+import TvIcon from "../images/icons/TvIcon.svg";
 
+import Link from "next/link";
 
-import Link from 'next/link';
+const Header_Mobile = (props) => {
+  const [navStatus, setNavStatus] = useState("");
+  const { setSearch } = props;
+  const [isShowSearch, seIsShowSearch] = useState(false);
 
-const Header_Mobile = () => {
+  let open_nav = () => {
+    setNavStatus("active");
+  };
+  let close_nav = () => {
+    setNavStatus("");
+  };
 
-    const [navStatus, setNavStatus] = useState('');
-
-    let open_nav = () =>{
-        setNavStatus('active')
-    }
-    let close_nav = () =>{
-        setNavStatus('')
-    }
-
-    return (
-        <>
-            <div id="header_mob" className='header_mob'>
-                <div className="container">
-                    <div className="logo">
-                        <Link href={process.env.NEXT_PUBLIC_DOMAIN_NAME}>
-                            <span>
-                                <Image src={Logo} alt="Logo" width={60} />
-                            </span>
-                        </Link>
-                    </div>
-                    <div className="right_col">
-                        <div className="search__icon">
-                            <Image src={SearchIcon} alt="Search icon" />
-                        </div>
-                        <div className="hamburger__icon" onClick={open_nav}>
-                            <Image src={HamburgerIcon} alt="Hamburger icon" />
-                        </div>
-                    </div>
-                </div>
-
+  return (
+    <>
+      <div id="header_mob" className="header_mob">
+        <div className="container">
+          <div className="logo">
+            <Link href={process.env.NEXT_PUBLIC_DOMAIN_NAME}>
+              <span>
+                <Image src={Logo} alt="Logo" width={60} />
+              </span>
+            </Link>
+          </div>
+          <div className="right_col">
+            {isShowSearch && (
+              <input
+                type="text"
+                id="search_input"
+                className="search_input"
+                placeholder="Buscar por canal, programa"
+                onChange={(e) => {
+                  setSearch(e.target.value);
+                  document.getElementById("search_input").focus = true;
+                }}
+              />
+            )}
+            <div className="search__icon">
+              <Image
+                src={SearchIcon}
+                alt="Search icon"
+                onClick={() => seIsShowSearch(!isShowSearch)}
+              />
             </div>
-
-            <div className={`nav_container ${navStatus}`}>
-
-                <div className="navigation">
-                    <div className="nav__head">
-                        
-                        <Image src={Logo} alt="Logo" width={45} height={28} />
-
-                        <div id="close_nav" onClick={close_nav}>
-                            <svg width="25" height="25" viewBox="0 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M18.75 6.25L6.25 18.75M6.25 6.25L18.75 18.75" stroke="#707A87" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
-                            </svg>
-                        </div>
-
-                    </div>
-
-                    <div className="nav_body">
-                        <Link href="#">
-                            <div className="item">
-                                <Image src={SearchIcon} alt="Search icon" />
-                                <span className='text'>Canal de búsqueda</span>
-                            </div>
-                        </Link>
-                        
-                        <Link href={process.env.NEXT_PUBLIC_DOMAIN_NAME+'/settings'}>
-                            <div className="item">
-                                <SettingsIcon />
-                                <span className='text'>Personalizar canal</span>
-                            </div>
-                        </Link>
-
-                        <Link href={process.env.NEXT_PUBLIC_DOMAIN_NAME+'/about-us'}>
-                            <div className="item">
-                                <Image src={TvIcon} alt="TV icon" />
-                                <span className='text'>Acerca de tv.cl</span>
-                            </div>
-                        </Link>
-                    </div>
-                </div>
-
+            <div className="hamburger__icon" onClick={open_nav}>
+              <Image src={HamburgerIcon} alt="Hamburger icon" />
             </div>
-        </>
-    )
-}
-export default Header_Mobile
+          </div>
+        </div>
+      </div>
+
+      <div className={`nav_container ${navStatus}`}>
+        <div className="navigation">
+          <div className="nav__head">
+            <Image src={Logo} alt="Logo" width={45} height={28} />
+
+            <div id="close_nav" onClick={close_nav}>
+              <svg
+                width="25"
+                height="25"
+                viewBox="0 0 25 25"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M18.75 6.25L6.25 18.75M6.25 6.25L18.75 18.75"
+                  stroke="#707A87"
+                  strokeWidth="1.5"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+              </svg>
+            </div>
+          </div>
+
+          <div className="nav_body">
+            <Link href="#">
+              <div className="item">
+                <Image src={SearchIcon} alt="Search icon" />
+                <span className="text">Canal de búsqueda</span>
+              </div>
+            </Link>
+
+            <Link href={process.env.NEXT_PUBLIC_DOMAIN_NAME + "/settings"}>
+              <div className="item">
+                <SettingsIcon />
+                <span className="text">Personalizar canal</span>
+              </div>
+            </Link>
+
+            <Link href={process.env.NEXT_PUBLIC_DOMAIN_NAME + "/about-us"}>
+              <div className="item">
+                <Image src={TvIcon} alt="TV icon" />
+                <span className="text">Acerca de tv.cl</span>
+              </div>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+export default Header_Mobile;
