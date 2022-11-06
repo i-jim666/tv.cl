48535492863fe5d8615c23722f2d325b6da70d45
i-jim666 <ishtiaque.jim@gmail.com>
2022-11-07 01:13:48 +0600

    Cookie policy updated

--- global-components/Footer.js
+++ global-components/Footer.js
@@ -13,11 +13,8 @@ const Footer = () => {
                     <Link href={process.env.NEXT_PUBLIC_DOMAIN_NAME+'/acerca-de'}>
                         <div className="link">Acerca de tv.cl</div>
                     </Link>
-                    <Link href={process.env.NEXT_PUBLIC_DOMAIN_NAME+'/cookie-policy'}>
-                        <div className="link">Cookie policy</div>
-                    </Link>
-                    <Link href={process.env.NEXT_PUBLIC_DOMAIN_NAME+'/terms'}>
-                        <div className="link">Terms</div>
+                    <Link href={process.env.NEXT_PUBLIC_DOMAIN_NAME+'/politica-de-cookies'}>
+                        <div className="link">Política de cookies</div>
                     </Link>
                 </div>
             </div>
