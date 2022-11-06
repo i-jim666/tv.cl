b01c6e31a86cde0a2b1e407c1e6bda714cdfac8a
i-jim666 <ishtiaque.jim@gmail.com>
2022-11-06 19:32:53 +0600

    Spelling fixed

--- pages/configuracion.js
+++ pages/configuracion.js
@@ -15,7 +15,7 @@ export default function Settings() {
   return (
     <div className={`main__wrapper`}>
       <HeadGlobal
-        title = "Configuracion - TV.cl"
+        title = "Configuración - TV.cl"
         description = "Mesa de TV para todos los canales. ¿Te preguntas qué hay en la televisión en este momento? ¿Buscas el horario de TV de mañana? TV.cl tiene una completa guía de TV para ti."
       />
 
