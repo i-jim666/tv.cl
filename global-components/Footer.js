af39d8d8a9224dead965d60ab0d0761175d036f3
i-jim666 <ishtiaque.jim@gmail.com>
2022-11-09 17:50:16 +0600

    Channel number fixed

--- global-components/Footer.js
+++ global-components/Footer.js
@@ -7,7 +7,7 @@ const Footer = () => {
             <div className="container">
                 <div className="left-col">
                     <div className="big_text">Acerca de tv.cl</div>
-                    <div className="footer_about_desc">tv.cl es una guía de televisión inteligente, agenda con la programación de más de 200 canales, disfruta lo mejor de la televisión.</div>
+                    <div className="footer_about_desc">tv.cl es una guía de televisión inteligente, agenda con la programación de más de 39 canales, disfruta lo mejor de la televisión.</div>
                 </div>
                 <div className="links">
                     <Link href={process.env.NEXT_PUBLIC_DOMAIN_NAME+'/acerca-de'}>
